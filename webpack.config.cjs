83aa5138610846146bdd1241dea00927bfebb60f
Abdullah A Abdulkarim <39080654+abdullahak@users.noreply.github.com>
2023-10-01 22:17:35 +0000

    Removed the browser error for loading files

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',  
-  entry: './src/server/index.js',
+  entry: './src/client/App.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public', 'dist'),
     filename: 'bundle.js',
   },
   module: {
